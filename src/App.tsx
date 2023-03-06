93aab11d8fe661a4ca0b29188b8965020ff66762
Zachary Johnson <zachary.moritz.johnson@gmail.com>
2023-03-05 22:44:33 -0500

    get all entries from the space

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { GetEntry } from './services/contentful/content-delivery-api'
+import { GetEntries } from './services/contentful/content-delivery-api'
 import { PhotoVoiceAppBar } from './components/app-bar/app-bar';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <PhotoVoiceAppBar />
-      <GetEntry />
+      <GetEntries />
     </div>
   )
 }
--- src/services/contentful/content-delivery-api.tsx
+++ src/services/contentful/content-delivery-api.tsx
@@ -1,35 +1,41 @@
 import React, { useState } from "react";
 import { VoiceEntry } from '../../shared/content-types';
-import { createClient } from 'contentful';
+import { createClient, Entry } from 'contentful';
 import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 
-export const GetEntry: React.FC = () => {
-    const [voiceEntry, setVoiceEntry] = useState<VoiceEntry | undefined>();
+export const GetEntries: React.FC = () => {
+    const [voiceEntries, setVoiceEntries] = useState<Entry<VoiceEntry>[] | undefined>();
     const client = createClient({
         space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
         environment: import.meta.env.VITE_CONTENTFUL_ENVIRONMENT_ID,
         accessToken: import.meta.env.VITE_CONTENTFUL_API_KEY
       })
-    
+
     // Add error handling
-    client.getEntry<VoiceEntry>(import.meta.env.VITE_FIRST_ENTRY_ID).then((entry) => setVoiceEntry(entry.fields));
+    client.getEntries<VoiceEntry>().then((entries) => setVoiceEntries(entries.items)).catch(console.error);
 
     return (
-        <Card sx={{ display: 'flex' }} variant="outlined">
-            <CardMedia component="img" sx={{ width: 1/2 }} image={voiceEntry?.photo[0].fields.file.url} alt={voiceEntry?.photo[0].fields.title} />
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }}>
-                    <Typography gutterBottom component="div" variant="h5">
-                        {voiceEntry?.title}
-                    </Typography>
-                    <Typography gutterBottom variant="body1" component="div">
-                        {voiceEntry?.japaneseVoice}
-                    </Typography>   
-                    <Typography variant="body2" component="div">
-                        {voiceEntry?.englishVoice}
-                    </Typography>
-                </CardContent>
-            </Box>
-        </Card>
+        <>
+            {voiceEntries?.map(entry => {
+                return (
+                    <Card sx={{ display: 'flex' }} variant="outlined">
+                        <CardMedia component="img" sx={{ width: 1/2 }} image={entry.fields.photo[0].fields.file.url} alt={"Photo Voice image"} />
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                <Typography gutterBottom component="div" variant="h5">
+                                    {entry.fields.title}
+                                </Typography>
+                                <Typography gutterBottom variant="body1" component="div">
+                                    {entry.fields.japaneseVoice}
+                                </Typography>
+                                <Typography variant="body2" component="div">
+                                    {entry.fields.englishVoice}
+                                </Typography>
+                            </CardContent>
+                        </Box>
+                    </Card> 
+                )
+            })}
+        </> 
     );
 }
