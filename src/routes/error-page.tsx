277efc4e207914159fc9edd33a9d996af565bb51
Zachary Johnson <zachary.moritz.johnson@gmail.com>
2023-04-12 19:07:22 -0400

    fix useRouteError type

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -1,16 +1,19 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <h1>Oops!</h1>
+        <h2>{error.status}</h2>
+        <p>{error.statusText}</p>
+        {error.data?.message && <p>{error.data.message}</p>}
+      </div>
+    );
+  } else {
+    return <div>Oops</div>;
+  }
 }
