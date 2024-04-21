69803646a57ca35ee42a4fefa0cf663f059742d1
Zachary Johnson <zachary.business@tutanota.com>
2024-04-20 21:44:06 -0400

    add first tests and add linting to test the github actions tasks setup (#7)
    * add first tests and add linting and testing to the github actions tasks

--- src/components/icon-display/icon-display.test.tsx
+++ src/components/icon-display/icon-display.test.tsx
@@ -1,9 +1,15 @@
-import { describe, expect, test } from "vitest";
 import { IconDisplay } from "./icon-display";
 import { render } from "@testing-library/react";
+import { mockVoiceEntries } from "../../../__mocks__/mock-data";
 
 describe("FilterVoiceEntries", () => {
   test("should render successfully", async () => {
+    vi.mock("react-router-dom", () => ({
+      ...vi.importActual("react-router-dom"),
+      useLoaderData: () => mockVoiceEntries,
+      useNavigate: () => vi.fn(),
+    }));
+
     const container = render(<IconDisplay />);
 
     expect(container).toBeTruthy();
